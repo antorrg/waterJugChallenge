6014f6c7db738e4b9e4ca02445fa27e30a034b77
Antonio R. Rodriguez Gramajo <114789901+antorrg@users.noreply.github.com>
2024-06-06 01:00:02 -0300

    Update 03.test.js

--- test/03.test.js
+++ test/03.test.js
@@ -3,7 +3,7 @@ const jugService = require('../src/services/jugService');
 describe('jugService', () => {
   it('should return the correct solution steps', () => {
     const result = jugService.createJugCases(2, 10, 4);
-    expect(result).toEqual({"solution": [
+    expect(result.solution).toEqual({"solution": [
       { step: 1, action: "Fill bucket X", bucketX: 2, bucketY: 0 },
       { step: 2, action: "Transfer from bucket X to Y", bucketX: 0, bucketY: 2 },
       { step: 3, action: "Fill bucket X", bucketX: 2, bucketY: 2 },
